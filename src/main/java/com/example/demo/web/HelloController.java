5b7c4cc20c7ab0602311ab397e979410cab7efbc
chenxx <chenxx@dida100.cn>
2018-06-07 16:38:45 +0800

    commit test

---
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "springBootTest";
+    }
+    @GetMapping("/WIN_10_UP")
+    public String test2(@RequestParam String showStr) {
+
+        return showStr;
+    }
+    @PostMapping("/postRequest")
+    public String requestTest() {
+
+
+        return  "";
+    }
+}
