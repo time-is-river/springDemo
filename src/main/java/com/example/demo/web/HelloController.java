8c7ed491c5f4aac850c7889274225716816f3002
chenxx <chenxx@dida100.cn>
2018-06-21 15:33:04 +0800

    v1.0 本地运行版本

--- src/main/java/com/example/demo/web/HelloController.java
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
+    @RequestMapping("/")
+    public String index(){
+        return "welcome";
+    }
     @RequestMapping("/hello")
     public String hello(){
         return "springBootTest";
